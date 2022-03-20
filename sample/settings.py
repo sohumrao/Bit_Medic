440e94be41f452dde9c25b0438ec7d2357a7eac8
Max Ratmeyer <17440617+mratmeyer@users.noreply.github.com>
2022-03-20 11:30:37 -0400

    Please work database

--- sample/settings.py
+++ sample/settings.py
@@ -86,7 +86,7 @@ if os.getenv('GAE_APPLICATION', None):
             'HOST': '34.86.79.245',
             'USER': 'bitmedic',
             'PASSWORD': os.getenv('SQL_PASSWORD'),
-            'NAME': 'bitmedic4',
+            'NAME': 'bitmedic5',
         }
     }
 else:
